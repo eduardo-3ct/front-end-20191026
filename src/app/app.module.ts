02a2eeb2a128f7d4bd173ca122b5402159f3632f
Eduardo Sánchez <eduardo@MacBook-Pro-de-Eduardo.local>
2020-02-08 10:07:31 -0600

    Módulos de características.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BeerListComponent } from './beer-list/beer-list.component';
-import { FavBeerComponent } from './fav-beer/fav-beer.component';
-import { BeerSearchComponent } from './beer-search/beer-search.component';
+import { FavBeerComponent } from './beer/fav-beer/fav-beer.component';
 import { RouterModule } from '@angular/router';
-import { BeerDetailComponent } from './beer-detail/beer-detail.component';
-import { BeerDetailGuard } from './beer-detail/beer-detail.guard';
+import { BeerModule } from './beer/beer.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BeerListComponent,
-    BeerSearchComponent,
-    FavBeerComponent,
-    BeerDetailComponent
+    AppComponent
   ],
   imports: [
     AppRoutingModule,
+    BeerModule,
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'home', component: FavBeerComponent },
       { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'beers', component: BeerSearchComponent },
-      { path: 'beers/:id', component: BeerDetailComponent, canActivate: [ BeerDetailGuard ] },
       { path: '**', redirectTo: '/home', pathMatch: 'full' },
     ])
   ],
